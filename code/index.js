26be31f58eae3cfe7655894fb34ae81a113e67fd
damikas23 <145433214+damikas23@users.noreply.github.com>
2023-11-01 20:49:01 +0000

    Add files via upload

---
+++ code/index.js
@@ -0,0 +1,26 @@
+const CodeRestriction = 'uwfuebq3uwidl'.trim();
+let ageButton = document.getElementsByTagName("button")[0];
+
+function getValue() {
+    let ageInput = document.getElementsByTagName("input")[0]
+    console.log(ageInput)
+    return ageInput.value
+};
+
+function checkRestriction() {
+    let code = getValue();
+
+    if (code == CodeRestriction) {
+        alert("!Correct code!")
+    } else {
+        alert("!Incorrect code!")
+    }
+}
+function displayResult() {
+    let result = checkRestriction();
+    console.log(result)
+    let pTag = document.getElementById("display")
+    pTag.textContent = result;
+}
+
+ageButton.onclick = displayResult
